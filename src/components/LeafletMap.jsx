6f423a8ca02c23e1de175b92f8417f07b0bc988a
DanStevensonCO <danstevensonco@gmail.com>
2021-03-30 20:39:35 +0100

    update image and map config

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
+import meteorImg from './meteor.png'
 
 class LeafletMap extends React.Component {
     
     render(){
         const { meteorites } = this.props
+        console.log(meteorites)
     
-        const position = [53.5, -2]
+        const position = [54.5, -2]
+        // const position = [51.783330, -1.783330]
 
         return (
             <div className="map-container">
@@ -27,23 +30,26 @@ class LeafletMap extends React.Component {
                             yearLanded = meteorite.year.slice(0, 4)
                         } 
 
+                        console.log(meteorite.name, yearLanded)
+
                         let mass = "Unknown"
 
                         if(meteorite.mass){
-                            mass = Math.round(meteorite.mass * 100) / 100
+                            mass = (Math.round(meteorite.mass * 100) / 100) /1000
                         } 
                         
                         let rotate = Math.random() * 360
+                        // let rotate = 45
 
                         let markerIcon = L.divIcon({
                             iconSize: [20, 20],
-                            iconAnchor: [45, 45],
+                            iconAnchor: [7, 60],
                             className: `meteor-${meteorite.id}`,
                             html: `<img 
-                            style="transform: rotate(${rotate}deg);"
-                            height="100" 
-                            width="100" 
-                            src='/static/media/meteor.a98d6299.png'>`
+                            style="transform: rotate(${rotate}deg); transform-origin: bottom center;"
+                            height="60" 
+                            width="13" 
+                            src=${meteorImg}>`
                         })
 
                         let moreInfoLink = "https://www.google.com/search?q=" + meteorite.name + " meteorite"
@@ -52,7 +58,7 @@ class LeafletMap extends React.Component {
                             <Marker position={markerPosition} icon={markerIcon}>
                             <Popup>
                                 <h3>{meteorite.name}</h3>
-                                <p><b>Mass:</b> {mass}</p>
+                                <p><b>Mass:</b> {mass}kg</p>
                                 <p><b>Year landed:</b> {yearLanded}</p>
                                 <p><a href={moreInfoLink} target="_blank">Learn more about this meteorite</a></p>
                             </Popup>
